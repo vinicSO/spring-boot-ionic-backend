4c0d317f10724c0f22a2ab0e59ed3e0ceae0ff2a
Vinicius Oliveira <vinicius.santos.oliv3ira@gmail.com>
2021-06-24 20:38:32 -0300

    Correções: update em CategoriaService e construtor de Pagamento

--- src/main/java/com/coursespring/cursomc/services/CategoriaService.java
+++ src/main/java/com/coursespring/cursomc/services/CategoriaService.java
@@ -36,8 +36,11 @@ public class CategoriaService {
     }
 
     public Categoria update(Categoria obj) {
-        find(obj.getId());
-        return categoriaRepository.save(obj);
+        Categoria newObj = find(obj.getId());
+
+        updateData(newObj, obj);
+
+        return categoriaRepository.save(newObj);
     }
 
     public void delete(Integer id) {
@@ -62,5 +65,9 @@ public class CategoriaService {
 
     public Categoria fromDTO(CategoriaDTO objDto) {
         return new Categoria(objDto.getId(), objDto.getNome());
+    }
+
+    private void updateData(Categoria newObj, Categoria obj) {
+        newObj.setNome(obj.getNome());
     }
 }
