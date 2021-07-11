7e727097bbb3a0216058c9ed87790ffa95136e5c
Vinícius Santos de Oliveira <vinicius.santos.oliv3ira@gmail.com>
2021-07-11 17:34:43 -0300

    Email HTML

--- src/main/java/com/coursespring/cursomc/services/EmailService.java
+++ src/main/java/com/coursespring/cursomc/services/EmailService.java
@@ -3,11 +3,15 @@ package com.coursespring.cursomc.services;
 import com.coursespring.cursomc.domain.Pedido;
 import org.springframework.mail.SimpleMailMessage;
 
+import javax.mail.internet.MimeMessage;
+
 public interface EmailService {
 
     void sendOrderConfirmationEmail(Pedido obj);
 
     void sendEmail(SimpleMailMessage msg);
 
+    void sendOrderConfirmationHtmlEmail(Pedido obj);
 
+    void sendHtmlEmail(MimeMessage msg);
 }
